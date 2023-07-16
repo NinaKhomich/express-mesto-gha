76a23d7a1bea26756f930962a7220ad996e2259a
NinaKhomich <kh22nina@gmail.com>
2023-07-16 21:07:10 +0300

    refactor: process.env, add helmet, rateLimit

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+
 const routes = require('./routes');
 const errorProcessing = require('./middlewares/error');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // за 15 минут
+  max: 100, // можно совершить максимум 100 запросов с одного IP
+});
+
+mongoose.connect(DB_URL);
 
 app.use(express.json());
+app.use(helmet());
+app.use(limiter);
 
 app.use(routes);
 app.use(errors());
